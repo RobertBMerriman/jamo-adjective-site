204a279ee0f7fdc243b6aae622ef21a11769f2d9
Robert Merriman <robertbmerriman@gmail.com>
2016-09-10 15:10:44 +0100

    Find and show adjective word (with basic error capturing)

--- app/controllers/root_controller.rb
+++ app/controllers/root_controller.rb
@@ -1,4 +1,42 @@
 class RootController < ApplicationController
+
+
   def index
+
+    adjective = Adjective.first
+    @word = error_check_adjective(adjective)
+
   end
+
+  private
+
+    # Small routine to check if the adjective is nil or blank
+    # and if the word is nil or blank
+    # Returns the correct word or the word as an error message if
+    def error_check_adjective(adjective)
+
+      # Check if adjective is nil or blank. If not, find word.
+      if adjective.nil?
+        word = "Internal Error: Adjective not found, please reload."
+        # TODO Throw error for this outcome
+      elsif adjective.blank?
+        word = "Internal Error: Adjective found is blank, please reload."
+        # TODO Throw error for this outcome
+      else
+        word = adjective.word # Find word
+
+        # Check if adjective is nil or blank. If not, correct word is returned
+        if word.nil?
+          word = "Internal Error: Word not found from adjective, please reload."
+          # TODO Throw error for this outcome
+        elsif word.blank?
+          word = "Internal Error: Word found is blank, please reload."
+          # TODO Throw error for this outcome
+        end
+
+      end
+
+      return word # Return word or error message
+    end
+
 end
